64e9bfa64ced1006bf608cf749691225d7b11495
Michael Boreham <bor3ham@gmail.com>
2016-11-14 12:32:36 +1100

    Working demo build setup

---
+++ site.js
@@ -0,0 +1,11 @@
+$('code.script').each(function(index, element) {
+  var jqElement = $(element);
+  var source = jqElement.data('source');
+  $.ajax({
+    url: source,
+    type: 'GET',
+    success: function(response) {
+      jqElement.text(response);
+    },
+  });
+});
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+
+module.exports = {
+  entry: './demo.jsx',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'demo.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015', 'stage-0']
+        }
+      }
+    ],
+  },
+  resolve: {
+    root: [
+    ],
+    extensions: [ '', '.js', '.jsx' ]
+  }
+};
