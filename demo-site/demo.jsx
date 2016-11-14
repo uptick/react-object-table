ed2cc6600688693c785bec5b2546f8e8e5df2dae
Michael Boreham <bor3ham@gmail.com>
2016-11-14 12:32:36 +1100

    Working demo build setup

---
+++ demo-site/demo.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+var mount = document.querySelectorAll('div.demo-mount');
+
+ReactDOM.render(<p>Blah</p>, mount[0]);
--- sample-code.jsx
+++
@@ -1,5 +0,0 @@
-console.log('blah');
-
-// import React from 'react'
-// import ReactDOM from 'react-dom'
-// ReactDOM.render();
