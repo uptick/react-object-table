a2e60840e48fb2d8c7ae072b829d030f69e688ce
Michael Boreham <bor3ham@gmail.com>
2016-11-17 12:36:39 +1100

    Version bump

--- demos.jsx
+++ demos.jsx
@@ -1,5 +1,3 @@
 import SimpleDemo from './simple-demo.jsx'
 import FeaturesDemo from './features-demo.jsx'
 import CustomisationDemo from './customisation-demo.jsx'
-
-console.log('test even MORE more jsx changes');
