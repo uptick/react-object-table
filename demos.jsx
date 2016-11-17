94830f8c19b325eaf8ce1010cc7d8b3476d68960
Michael Boreham <bor3ham@gmail.com>
2016-11-17 11:59:05 +1100

    More command tests

--- demos.jsx
+++ demos.jsx
@@ -1,3 +1,5 @@
 import SimpleDemo from './simple-demo.jsx'
 import FeaturesDemo from './features-demo.jsx'
 import CustomisationDemo from './customisation-demo.jsx'
+
+console.log('test more jsx changes');
