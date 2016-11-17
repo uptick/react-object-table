c6a5a420b3f716bcf59eff49ebf27afba4ddb50a
Michael Boreham <bor3ham@gmail.com>
2016-11-17 15:00:06 +1100

    Split demo page content into npm package

--- demo-site/demos.jsx
+++ demo-site/demos.jsx
@@ -1,3 +1,7 @@
+import { init } from 'abas-demo-site'
+
 import SimpleDemo from './simple-demo.jsx'
 import FeaturesDemo from './features-demo.jsx'
 import CustomisationDemo from './customisation-demo.jsx'
+
+init();
