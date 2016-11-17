5d57f1b165f359e179db27e4888984bff74f2025
Michael Boreham <bor3ham@gmail.com>
2016-11-17 12:01:02 +1100

    Shameful commits, I know

--- demo-site/demos.jsx
+++ demo-site/demos.jsx
@@ -2,4 +2,4 @@ import SimpleDemo from './simple-demo.jsx'
 import FeaturesDemo from './features-demo.jsx'
 import CustomisationDemo from './customisation-demo.jsx'
 
-console.log('test more jsx changes');
+console.log('test even MORE more jsx changes');
