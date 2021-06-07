9636bd0aba582dbbf36c7137f122c0ebc4d4bec4
umi wimalawardana <dreadera@outlook.com>
2021-06-07 12:28:00 +1000

    Cleaning Up.

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import ObjectTable, { ObjectCell, ObjectRow, BaseEditor, TextEditor, TextDrawer } from './object-table'
+
+export default ObjectTable
+export { ObjectCell, ObjectRow, BaseEditor, TextEditor, TextDrawer }
